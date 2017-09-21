d91c52f47ca5e380a95da8ade9cf07dff2a86629
Jo Lam <tsimehcppa@gmail.com>
2017-09-21 19:59:53 +0900

    First Commit

---
+++ app/src/main/java/com/hasuka/konsapo/CustomTabLayout.java
@@ -0,0 +1,99 @@
+package com.hasuka.konsapo;
+
+import android.app.Activity;
+import android.support.design.widget.TabLayout;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by lamyiucho on 13/9/2017.
+ */
+
+public class CustomTabLayout extends TabLayout {
+
+//    private static final String TAG = CustomTabLayout.class.getSimpleName();
+    private static final int WIDTH_INDEX = 0;
+    private static final int DIVIDER_FACTOR = 3;
+    private static final String SCROLLABLE_TAB_MIN_WIDTH = "mScrollableTabMinWidth";
+
+    public CustomTabLayout(Context context) {
+        super(context);
+        initTabMinWidth();
+    }
+
+    public CustomTabLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initTabMinWidth();
+    }
+
+    public CustomTabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initTabMinWidth();
+    }
+
+//    @Override
+//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//        if (getTabCount() == 0) {
+//            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//            return;
+//        }
+//
+//        try {
+//            ViewGroup tabLayout = (ViewGroup)getChildAt(0);
+//            int widthOfAllTabs = 0;
+//            for (int i = 0; i < tabLayout.getChildCount(); i++) {
+//                widthOfAllTabs += tabLayout.getChildAt(i).getMeasuredWidth();
+//            }
+//
+//            int measuredWidth = this.getScreenWidth(getContext());
+//
+//            setTabMode(widthOfAllTabs < measuredWidth ? MODE_FIXED : MODE_SCROLLABLE);
+//            setTabGravity(widthOfAllTabs <= measuredWidth ? TabLayout.GRAVITY_FILL : TabLayout.GRAVITY_FILL);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//    }
+//        @Override
+//        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//            try {
+//                if (getTabCount() == 0)
+//                    return;
+//                Field field = TabLayout.class.getDeclaredField("mTabMinWidth");
+//                field.setAccessible(true);
+//                field.set(this, (int) (getMeasuredWidth() / (float) getTabCount()));
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//}
+//
+//    public static int getScreenWidth(Context context) {
+//        DisplayMetrics metrics = new DisplayMetrics();
+//        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(metrics);
+//
+//        return metrics.widthPixels;
+//    }
+    private void initTabMinWidth() {
+        int[] wh = Utils.getScreenSize(getContext());
+        int tabMinWidth = wh[WIDTH_INDEX] / DIVIDER_FACTOR;
+
+        Field field;
+        try {
+            field = TabLayout.class.getDeclaredField(SCROLLABLE_TAB_MIN_WIDTH);
+            field.setAccessible(true);
+            field.set(this, tabMinWidth);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/hasuka/konsapo/HomeActivity.java
@@ -0,0 +1,372 @@
+package com.hasuka.konsapo;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.CookieSyncManager;
+import android.webkit.HttpAuthHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+import android.webkit.CookieManager;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+import java.net.HttpCookie;
+import java.util.*;
+public class HomeActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+//    private int[] tabTitles = {R.string.tab_home,R.string.tab_search,R.string.tab_new,R.string.tab_bookmark,R.string.tab_message};
+    private int[] tabTitles = {R.string.tab_home,R.string.tab_search,R.string.tab_new};
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+    private CustomTabLayout tabLayout;
+
+//    private int[] tabIcons = {
+//            R.drawable.ic_action_home,
+//            R.drawable.ic_action_search,
+//            R.drawable.ic_action_new,
+//            R.drawable.ic_action_bookmark,
+//            R.drawable.ic_action_message
+//    };
+    private int[] tabIcons = {
+            R.drawable.ic_action_home,
+            R.drawable.ic_action_search,
+            R.drawable.ic_action_new
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Fabric.with(this, new Crashlytics());
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().hide();
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        List<String> urls = new ArrayList<String>();
+        urls.add("https://konkatsu10.com/");
+        urls.add("https://konkatsu10.com/index.php?app_controller=search&type=mid&run=true");
+        urls.add("https://konkatsu10.com/index.php?app_controller=search&type=news&run=true&category=news&category_PAL[]=match%20comp");
+//        urls.add("https://konkatsu10.com/index.php?app_controller=search&type=clip&run=true&pal=mid");
+//        urls.add("https://konkatsu10.com/index.php?app_controller=search&type=message&run=true&pal=rec");
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(),urls);
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOffscreenPageLimit(3);
+        tabLayout = (CustomTabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        setupTabIcons();
+
+    }
+
+    private void setupTabIcons() {
+
+        for(int i = 0; i < 3;i++){
+            LinearLayout tabLinearLayout = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.custom_tab, null);
+            TextView tabContent = (TextView) tabLinearLayout.findViewById(R.id.tabContent);
+            tabContent.setText("  "+getApplicationContext().getResources().getString(tabTitles[i]));
+            tabContent.setCompoundDrawablesWithIntrinsicBounds(tabIcons[i],0, 0, 0);
+            tabLayout.getTabAt(i).setCustomView(tabContent);
+
+        }
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_URL = "section_url";
+
+        private WebView webView;
+        private String mUrl;
+        private View rootView;
+        private ProgressBar progressBar;
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(String sectionUrl) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putString(ARG_SECTION_URL, sectionUrl);
+            fragment.setArguments(args);
+
+            return fragment;
+        }
+
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//            setContentView(rootView);
+//            setContentShown(false);
+            super.onActivityCreated(savedInstanceState);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            rootView = inflater.inflate(R.layout.fragment_home, container, false);
+            if (webView != null) {
+                webView.destroy();
+            }
+
+            progressBar = (ProgressBar) rootView.findViewById(R.id.fragment_photo_page_progress_bar);
+            progressBar.setMax(100);
+
+            mUrl = getArguments().getString(ARG_SECTION_URL);
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptCookie(true);
+            cookieManager.acceptCookie();
+            cookieManager.setAcceptFileSchemeCookies(true);
+
+            SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+            String cookiesValue = "";
+            sharedPref.getString("cookies",cookiesValue);
+            if (cookiesValue.length()>0){
+                cookieManager.setCookie(mUrl,cookiesValue);
+            }else{
+                cookieManager.getCookie(mUrl);
+            }
+
+
+
+            webView = (WebView) rootView.findViewById(R.id.webview);
+            webView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    return true;
+                }
+            });
+            webView.setLongClickable(false);
+
+            webView.setWebChromeClient(new WebChromeClient(){
+                @Override
+                public void onProgressChanged(WebView view, int newProgress) {
+                    super.onProgressChanged(view, newProgress);
+                    if (newProgress == 100) {
+                        progressBar.setVisibility(View.GONE);
+                    } else {
+                        progressBar.setVisibility(View.VISIBLE);
+                        progressBar.setProgress(newProgress);
+                    }
+                }
+            });
+
+            webView.setWebViewClient(new WebViewClient(){
+
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                    CookieManager cookieManager = CookieManager.getInstance();
+                    String cookies = cookieManager.getCookie(mUrl);
+                    SharedPreferences settings = getActivity().getPreferences(Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString("cookies",cookies);
+                    editor.commit();
+                    return false;
+                }
+
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                    super.onPageStarted(view, url, favicon);
+
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                }
+
+                @Override
+                public void onReceivedHttpAuthRequest(WebView view, HttpAuthHandler handler, String host, String realm) {
+                    handler.proceed(getString(R.string.USERNAME), getString(R.string.PASSWORD));
+                    super.onReceivedHttpAuthRequest(view, handler, host, realm);
+                }
+            });
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webSettings.setSupportZoom(false);
+            webSettings.setBuiltInZoomControls(false);
+            webSettings.setUseWideViewPort(false);
+            webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+//            webSettings.setLoadWithOverviewMode(true);
+            webView.setHorizontalScrollBarEnabled(false);
+            webView.loadUrl(mUrl);
+
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private List<String> mDatas;
+
+
+        public SectionsPagerAdapter(FragmentManager fm, List<String> datas) {
+            super(fm);
+            this.mDatas = datas;
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+            return super.getItemPosition(object);
+        }
+
+        @Override
+        public void notifyDataSetChanged() {
+            super.notifyDataSetChanged();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            String url = mDatas.get(position);
+//            PlaceholderFragment fm = new PlaceholderFragment(url);
+            return PlaceholderFragment.newInstance(url);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 5 total pages.
+            return tabIcons.length;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            super.destroyItem(container, position, object);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+//            switch (position) {
+//                case 0:
+//                    return "ホーム";
+//                case 1:
+//                    return "検索";
+//                case 2:
+//                    return "新着";
+//                case 3:
+//                    return "お気に入り";
+//                case 4:
+//                    return "お知らせ";
+//
+//            }
+            return null;
+        }
+
+
+
+
+    }
+
+
+
+    public class CustomWebViewClient extends WebViewClient{
+
+        private String url = "";
+        private Context context;
+
+        public CustomWebViewClient(String url,Context context) {
+            super();
+            this.url = url;
+            this.context = context;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+
+        }
+    }
+
+
+}
